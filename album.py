381df03e93d052c34a1f8f7b70ecefbcfe333ec8
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-06 18:06:36 -0300

    Update album.py

--- album.py
+++ album.py
@@ -21,4 +21,54 @@ class Album:
             return False 
         
         novo_nodo = NodoLista(figurinha)
+         # Se o álbum está vazio, o novo nó se torna a cabeça
+        if self.cabeca is None:
+            self.cabeca = novo_nodo
+        else:
+            # Percorre a lista até achar o último nó (onde 'proximo' é None)
+            atual = self.cabeca
+            while atual.proximo is not None:
+                atual = atual.proximo
+            # Faz o último nó apontar para o novo
+            atual.proximo = novo_nodo
+            
+        self.tamanho += 1
+        return True
+
+    def remover(self, id_figurinha: int) -> bool:
+        """
+        Busca e remove uma figurinha pelo ID.
+        Retorna True se removeu, ou False se não encontrou.
+        """
+        atual = self.cabeca
+        anterior = None
         
+        while atual is not None:
+            if atual.figurinha.id == id_figurinha:
+                # Se a figurinha a ser removida for a primeira (cabeça)
+                if anterior is None:
+                    self.cabeca = atual.proximo
+                # Se estiver no meio ou no final
+                else:
+                    anterior.proximo = atual.proximo
+                
+                self.tamanho -= 1
+                return True # Removida com sucesso
+            
+            # Avança os ponteiros
+            anterior = atual
+            atual = atual.proximo
+            
+        return False # Figurinha não encontrada
+
+    def buscar(self, id_figurinha: int) -> Figurinha:
+        """
+        Busca específica pelo número (ID) da figurinha.
+        """
+        atual = self.cabeca
+        while atual is not None:
+            if atual.figurinha.id == id_figurinha:
+                return atual.figurinha
+            atual = atual.proximo
+        return None
+       
