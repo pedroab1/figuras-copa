efce56b6b2d4695dd5cf0ed4698cf2e1df504663
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-06 18:04:58 -0300

    Update nodofila.py

---
+++ nodofila.py
@@ -0,0 +1,10 @@
+from figuras import Figurinha
+
+class NodoFila:
+    def __init__(self, figurinha: Figurinha):
+        """
+        Nó para a Fila FIFO (Trocas e Histórico).
+        Guarda a figurinha e o ponteiro para o próximo nó na fila.
+        """
+        self.figurinha = figurinha
+        self.proximo = None
