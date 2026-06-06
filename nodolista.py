0c4f57f0e2309167d511bb9554f9dc80e41ca58c
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-06 18:04:45 -0300

    Update nodolista.py

--- nodolista.py
+++ nodolista.py
@@ -2,3 +2,9 @@ from figuras import Figurinha
 
 class NodoLista:
     def __init__(self, figurinha: Figurinha):
+        """
+        Nó para a lista encadeada (Álbum).
+        Guarda a figurinha e o ponteiro para o próximo nó.
+        """
+        self.figurinha = figurinha
+        self.proximo = None
