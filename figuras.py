e4df55517188c89ac303fa8e4a0fa848cb6a304f
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-06 18:09:40 -0300

    Update figuras.py

---
+++ figuras.py
@@ -0,0 +1,6 @@
+class Figurinha:
+    def __init__(self, id_figurinha: int, nome: str, pais: str, posicao: str, raridade: str):
+        """
+        Inicializa uma nova figurinha com os atributos exigidos pelo projeto.
+        """
+        self.id = id_figurinha
