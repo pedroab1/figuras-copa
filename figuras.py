1e11d97296b85e5d96eae1aca4dbbaac5495f808
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-06 18:09:51 -0300

    Update figuras.py

--- figuras.py
+++ figuras.py
@@ -4,3 +4,14 @@ class Figurinha:
         Inicializa uma nova figurinha com os atributos exigidos pelo projeto.
         """
         self.id = id_figurinha
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
+
+    def __str__(self):
+        """
+        Define como a figurinha será exibida ao usarmos a função print().
+        Isso facilitará muito a visualização no terminal depois.
+        """
+        return f"[{self.id}] {self.nome} ({self.pais}) - {self.posicao} | {self.raridade}"
