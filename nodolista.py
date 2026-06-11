531dc39f6d0c4978e31ffccfe10f8f1c2ecf8d5d
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-06-10 21:21:27 -0300

    commit

--- nodolista.py
+++ nodolista.py
@@ -2,3 +2,9 @@ from figuras import Figurinha
 
 class NodoLista:
     def __init__(self, figurinha: Figurinha):
+        """
+        Nó para a Fila FIFO (Trocas e Histórico).
+        Guarda a figurinha e o ponteiro para o próximo nó na fila.
+        """
+        self.figurinha = figurinha
+        self.proximo = None
